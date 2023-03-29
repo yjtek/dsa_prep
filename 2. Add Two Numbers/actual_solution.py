8a9d4c25a8d44338567e25da7e347ffe4db7c892
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-03-29 15:14:04 +0800

    push changes

--- 2. Add Two Numbers/actual_solution.py
+++ 2. Add Two Numbers/actual_solution.py
@@ -31,4 +31,32 @@ class ListNode:
         self.val = val
         self.next = next
 '''
+from typing import Optional, Type
 
+class ListNode:
+    def __init__(self, val=0, next: Optional[Type['ListNode']] = None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2:
+
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            s = x + y + carry
+            carry = s // 10
+            
+            curr.next = ListNode(s % 10)
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            
+        if carry:
+            curr.next = ListNode(carry)
+            
+        return dummy.next
