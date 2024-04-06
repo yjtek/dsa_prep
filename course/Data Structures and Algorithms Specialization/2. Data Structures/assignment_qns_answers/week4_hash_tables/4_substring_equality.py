e6256a23c553d141bc675ae748b24e31f07c60ee
yongjian.tek <yongjian.tek@grabtaxi.com>
2024-04-06 13:10:41 +0800

    sync

--- course/Data Structures and Algorithms Specialization/2. Data Structures/assignment_qns_answers/week4_hash_tables/4_substring_equality.py
+++ course/Data Structures and Algorithms Specialization/2. Data Structures/assignment_qns_answers/week4_hash_tables/4_substring_equality.py
@@ -5,11 +5,15 @@ import sys
 class Solver:
     def __init__(self, s) -> None:
         self.s: str = s
-        self.polynomial=10
-        self.prime1 = 1e9+7
-        self.prime2 = 1e9+9
-        self.precompute_hash_1 = self.compute_hash(string=s, polynomial=self.polynomial, prime=self.prime1)
-        self.precompute_hash_2 = self.compute_hash(string=s, polynomial=self.polynomial, prime=self.prime2)
+        self.polynomial=int(10)
+        self.prime1 = int(1e9+7)
+        self.prime2 = int(1e9+9)
+        self.precompute_hash_1 = self.compute_hash(
+            string=s, polynomial=self.polynomial, prime=self.prime1
+        )
+        self.precompute_hash_2 = self.compute_hash(
+            string=s, polynomial=self.polynomial, prime=self.prime2
+        )
 
     def _ask(self, a, b, l):
         return self.s[a:(a+l)] == self.s[b:(b+l)]
@@ -20,8 +24,8 @@ class Solver:
             hashval += (((polynomial * hashval) % prime) + ord(char)) % prime
         return hashval
     
-    def compute_hash(self, string, polynomial=10, prime=1e9+7):
-        precompute_substring_hash = [0.] * (len(string)+1)
+    def compute_hash(self, string, polynomial, prime):
+        precompute_substring_hash = [int(0)] * (len(string)+1)
         for i in range(len(string)):
             precompute_substring_hash[i+1] = (
                 (precompute_substring_hash[i] * polynomial) % prime + ord(string[i])
@@ -30,19 +34,40 @@ class Solver:
         return precompute_substring_hash
 
     def ask(self, a, b, l):
-        polynomial_multiple1 = (self.polynomial**l) % self.prime1
-        polynomial_multiple2 = (self.polynomial**l) % self.prime2
+        polynomial_multiple1 = pow(int(self.polynomial), int(l), int(self.prime1))
+        polynomial_multiple2 = pow(int(self.polynomial), int(l), int(self.prime2))
         
-        hash1_a = (self.precompute_hash_1[a+l] - (polynomial_multiple1 * self.precompute_hash_1[a]) % self.prime1) % self.prime1
-        hash2_a = (self.precompute_hash_2[a+l] - (polynomial_multiple2 * self.precompute_hash_2[a]) % self.prime2) % self.prime2
+        hash1_a = (
+            self.prime1 + 
+            self.precompute_hash_1[a+l] - 
+            ((polynomial_multiple1 * self.precompute_hash_1[a]) % self.prime1)
+         ) % self.prime1
+        
+        # hash2_a = (
+        #     self.prime2 + 
+        #     self.precompute_hash_2[a+l] - 
+        #     ((polynomial_multiple2 * self.precompute_hash_2[a]) % self.prime2)
+        # ) % self.prime2
 
-        hash1_b = (self.precompute_hash_1[b+l] - (polynomial_multiple1 * self.precompute_hash_1[b]) % self.prime1) % self.prime1
-        hash2_b = (self.precompute_hash_2[b+l] - (polynomial_multiple2 * self.precompute_hash_2[b]) % self.prime2) % self.prime2
+        hash1_b = (
+            self.prime1 + 
+            self.precompute_hash_1[b+l] - 
+            ((polynomial_multiple1 * self.precompute_hash_1[b]) % self.prime1)
+        ) % self.prime1  
+
+        # hash2_b = (
+        #     self.prime2 + 
+        #     self.precompute_hash_2[b+l] - 
+        #     ((polynomial_multiple2 * self.precompute_hash_2[b]) % self.prime2)
+        # ) % self.prime2      
         
+        strings_match = False
         hash1_match = hash1_a == hash1_b
-        hash2_match = hash2_a == hash2_b
+        if hash1_match:
+            strings_match = self.s[a:(a+l)] == self.s[b:(b+l)]
+        # hash2_match = hash2_a == hash2_b
 
-        return True if hash1_match and hash2_match else False
+        return True if (hash1_match and strings_match) else False
     
 s = sys.stdin.readline()
 q = int(sys.stdin.readline())
--- course/Data Structures and Algorithms Specialization/2. Data Structures/assignment_qns_answers/week4_hash_tables/5_common_substring.py
+++ course/Data Structures and Algorithms Specialization/2. Data Structures/assignment_qns_answers/week4_hash_tables/5_common_substring.py
@@ -4,8 +4,8 @@ import sys
 from collections import namedtuple
 
 Answer = namedtuple('answer_type', 'i j len')
-POLYNOMIAL=10
-PRIME1=1e9+7
+POLYNOMIAL=int(10)
+PRIME1=int(1e9+7)
 
 # def solve(s, t):
 # 	ans = Answer(0, 0, 0)
@@ -16,49 +16,67 @@ PRIME1=1e9+7
 # 					ans = Answer(i, j, l)
 # 	return ans
 
+def naive_solve(s,t):
+	ans = Answer(0, 0, 0)
+	for i in range(len(s)):
+		for j in range(len(t)):
+			for l in range(min(len(s) - i, len(t) - j) + 1):
+				if (l > ans.len) and (s[i:i+l] == t[j:j+l]):
+					ans = Answer(i, j, l)
+	return ans
+
 def precompute_cumulative_hash(string, prime):
     '''
     See Q4 for computation. Basically we use the relation that for string `s`, the hash `H()` of s[2:4] is simply H(s[0:4]) - H(s[0:2]) * x^2
     '''
-    hashvals_arr = [0.] * (len(string)+1)
+    hashvals_arr = [int(0)] * (len(string)+1)
     for i in range(len(string)):
-        hashvals_arr[i+1] = (((hashvals_arr[i] * POLYNOMIAL) % prime) + ord(string[i])) % prime
+        hashvals_arr[i+1] = int(
+            (((hashvals_arr[i] * POLYNOMIAL) % prime) + ord(string[i]))
+            % prime
+        )
     return hashvals_arr
 
-def compute_substring_hashvals(string, string_cumulative_hash_arr, substring_len, prime):
+def get_hashmap_for_substrings(string, string_cumulative_hash, substring_len, prime):
+    '''
+    Returns a map with the hashvalues as keys, and substring starting indices as values.
+
+    Note that the formula provided in the course is not accurate. Because there is a subtraction term, it is possible that you can end up with a negative number. As such, always add `prime` to the final sum to ensure that you are dealing with positive values. Adding `prime` doesn't change the final modulo, because prime % prime = 0, and won't affect the final answer by modular arithmetic.
+    '''
     # substr_hashes = [0.] * (len(string)-substring_len+1)
     substr_hash_map = {}
-    polynomial_term = 1
-    for _ in range(substring_len):
-        polynomial_term = (polynomial_term * POLYNOMIAL) % prime
+    polynomial_term = pow(int(POLYNOMIAL), int(substring_len), int(prime))
 
     for substring_start_index in range(len(string)-substring_len+1):
         substring_end_index = substring_start_index+substring_len
-        hashval = (
-            string_cumulative_hash_arr[substring_end_index] - 
-            (string_cumulative_hash_arr[substring_start_index] * polynomial_term)
-        ) % prime
+        hashval = int((
+            ##add `prime` to avoid negative values!!
+            prime +
+            string_cumulative_hash[substring_end_index] - 
+            ((string_cumulative_hash[substring_start_index] * polynomial_term) % prime)
+        ) % prime)
+        # print(f'{substring_start_index}, {substring_end_index}, {string[substring_start_index:substring_end_index]}, {hashval}')
         if hashval in substr_hash_map:
             substr_hash_map[hashval].append(substring_start_index) 
         else:
             substr_hash_map[hashval] = [substring_start_index]
     return substr_hash_map
 
-def get_common_substring_index(string, string_cumulative_hash_arr, hash_to_compare, substring_len, prime):
+def get_common_substring_index(string, string_cumulative_hash, string_to_compare, hash_to_compare, substring_len, prime):
     # substr_hashes = [0.] * (len(string)-substring_len+1)
-    substr_hash_map = {}
-    polynomial_term = 1
-    for _ in range(substring_len):
-        polynomial_term = (polynomial_term * POLYNOMIAL) % prime
+    polynomial_term = pow(int(POLYNOMIAL), int(substring_len), int(prime))
 
     for substring_start_index in range(len(string)-substring_len+1):
         substring_end_index = substring_start_index+substring_len
         hashval = (
-            string_cumulative_hash_arr[substring_end_index] - 
-            (string_cumulative_hash_arr[substring_start_index] * polynomial_term)
+            string_cumulative_hash[substring_end_index] - 
+            ((string_cumulative_hash[substring_start_index] * polynomial_term) % prime) + 
+            prime
         ) % prime
         if hashval in hash_to_compare:
-            return hash_to_compare.get(hashval)[0], substring_start_index
+            for index in hash_to_compare.get(hashval):
+                if string[substring_start_index:(substring_start_index+substring_len)] == string_to_compare[index:(index+substring_len)]:
+                    return index, substring_start_index
     return -1, -1
 
 def solve(s, t):
@@ -66,26 +84,39 @@ def solve(s, t):
     max_common_substr_len = min(len(s), len(t))
     min_common_substr_len = 0
     max_matching_substring_len = 0
-    string1_match_index = 0
-    string2_match_index = 0
+    string1_match_index, string2_match_index = -1, -1
+    
+    s_hashval_arr_p1 = precompute_cumulative_hash(s, PRIME1)
+    t_hashval_arr_p1 = precompute_cumulative_hash(t, PRIME1)
+    # s_hashval_arr_p2 = precompute_cumulative_hash(s, PRIME2)
+    # t_hashval_arr_p2 = precompute_cumulative_hash(t, PRIME2)
 
     while max_common_substr_len >= min_common_substr_len:
         # print('='*50)
-        # print(f"{max_common_substr_len=}, {min_common_substr_len=}")
-        check_length = (min_common_substr_len + max_common_substr_len)//2
-        s_hashval_arr_p1 = precompute_cumulative_hash(s, PRIME1)
-        t_hashval_arr_p1 = precompute_cumulative_hash(t, PRIME1)
+        check_length: int = (min_common_substr_len + max_common_substr_len)//2
+        # print(f"{max_common_substr_len=}, {min_common_substr_len=}, {check_length=}")
 
-        s_substring_hash_p1 = compute_substring_hashvals(s, s_hashval_arr_p1, check_length, PRIME1)
-        common_substring_string1_index, common_substring_string2_index  = get_common_substring_index(t, t_hashval_arr_p1, s_substring_hash_p1, check_length, PRIME1)
-        has_common_substring = common_substring_string1_index != -1
-        # print(s[common_substring_string1_index:(common_substring_string1_index+check_length)], t[common_substring_string2_index:(common_substring_string2_index+check_length)])
-
-        if has_common_substring:
+        s_substring_hash_p1 = get_hashmap_for_substrings(s, s_hashval_arr_p1, check_length, PRIME1)
+        
+        ## For debugging
+        t_substring_hash_p1 = get_hashmap_for_substrings(t, t_hashval_arr_p1, check_length, PRIME1)
+        
+        has_common_substring_p1 = False
+        common_substring_string1_index_p1, common_substring_string2_index_p1  = get_common_substring_index(string=t, string_cumulative_hash=t_hashval_arr_p1, string_to_compare=s, hash_to_compare=s_substring_hash_p1, substring_len=check_length, prime=PRIME1)
+        
+        # print(common_substring_string1_index_p1, common_substring_string2_index_p1)
+        common_hash_found = common_substring_string1_index_p1 != -1
+        is_not_collision = s[common_substring_string1_index_p1:(common_substring_string1_index_p1+check_length)] == t[common_substring_string2_index_p1:(common_substring_string2_index_p1+check_length)]
+        # print(f'{common_hash_found=}, {is_not_collision=}')
+        if common_hash_found and is_not_collision:
+            has_common_substring_p1 = True
+        
+        if has_common_substring_p1:
+            # print(s[common_substring_string1_index_p1:(common_substring_string1_index_p1+check_length)], t[common_substring_string2_index_p1:(common_substring_string2_index_p1+check_length)])
             max_matching_substring_len = check_length
             min_common_substr_len = check_length+1
-            string1_match_index = common_substring_string1_index
-            string2_match_index = common_substring_string2_index
+            string1_match_index = common_substring_string1_index_p1
+            string2_match_index = common_substring_string2_index_p1
         else:
             max_common_substr_len = check_length-1
         # print(f"{max_common_substr_len=}, {min_common_substr_len=}")
